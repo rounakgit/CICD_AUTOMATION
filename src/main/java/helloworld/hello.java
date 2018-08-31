fb498fd93689c5e388134d8c969f1e988ac53e03
rounakgit <42875653+rounakgit@users.noreply.github.com>
2018-09-01 00:05:38 +0530

    test

---
+++ src/main/java/helloworld/hello.java
@@ -0,0 +1,30 @@
+package helloworld;
+
+
+import java.io.*;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+
+@SuppressWarnings("serial")
+public class hello extends HttpServlet {
+  public void doGet (HttpServletRequest req,
+                     HttpServletResponse res)
+    throws ServletException, IOException
+  {
+    PrintWriter out = res.getWriter();
+
+    out.println("Hello, world!");
+    out.close();
+  }
+}
+
+/*public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Automation Build and deployment!" );
+	}
+
+}*/
